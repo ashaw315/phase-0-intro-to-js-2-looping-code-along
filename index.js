8e1c37183d3667729d7d195fe19a41786196d09b
Adam <ashaw315@gmail.com>
2021-11-01 12:54:40 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let newArray = [];
+    for(let i = 0; i < name.length; i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+
+function countDown(){
+    for (let countDown = 10; countDown > -1; countDown--) {
+        console.log(countDown);
+    }
+}
